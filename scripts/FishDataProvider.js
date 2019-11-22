c691b364a1216f7fb63263db54dc558c9e767797
jansenv <jansenv44@gmail.com>
2019-11-22 10:53:05 -0600

    Last save before the great abyss

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,56 @@
+
+
+const fishCollection = [
+    {
+        name: "Bitey",
+        species: "Piranha",
+        location: "Brazil",
+        length: 4,
+        food: "Humans",
+        image: "pics/piranha.png",
+    },
+    {
+        name: "Jawsie",
+        species: "Shark",
+        location: "South Africa",
+        length: 264,
+        food: "Seals",
+        image: "pics/left-shark.jpg",
+    },
+    {
+        name: "Mr. Bubbles",
+        species: "Goldfish",
+        location: "PetSmart",
+        length: 2,
+        food: "Flakes",
+        image: "pics/goldie-fish.jpg",
+    },
+    {
+        name: "Mrs. Dangles",
+        species: "Angler Fish",
+        location: "Mariana Trench",
+        length: 12,
+        food: "Minnows",
+        image: "pics/angler.jpg",
+    },
+    {
+        name: "Barry",
+        species: "Barracuda",
+        location: "Coral Reefs",
+        length: 70,
+        food: "Salmon",
+        image: "pics/barry.jpg",
+    },
+    {
+        name: "Snappy",
+        species: "Red Snapper",
+        location: "Gulf of Mexico",
+        length: 30,
+        food: "Eels",
+        image: "pics/red-snapper.jpg",
+    },
+]
+
+export const useFish = () => {
+    return fishCollection;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,13 @@
 import initializeDetailButtonEvents from './dialogs.js'
 import addMouseOverEventListeners from '../highlight.js'
 import addMouseOverEventListenersAside from './addBorder.js'
+import { useFish } from './FishDataProvider.js'
 
 initializeDetailButtonEvents()
 addMouseOverEventListeners()
 addMouseOverEventListenersAside()
+const allTheFish = useFish();
+
+for (const fish of allTheFish) {
+    console.log("individual fish object:", fish);
+}

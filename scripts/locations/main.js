ed6b55ab0cc00e135ef4e8115dbb526178ad6715
jansenv <jansenv44@gmail.com>
2019-12-07 23:49:43 -0600

    most recent changes

--- scripts/locations/main.js
+++ scripts/locations/main.js
@@ -2,6 +2,7 @@
  *  Import the initializeDetailButtonEvents function
  *  reference and then invoke it
  */
+
 import initializeDetailButtonEvents from "./dialogs.js";
 import addMouseOverEventListeners from "../fish/highlight.js";
 import addMouseOverEventListenersAside from "../fish/addBorder.js";
